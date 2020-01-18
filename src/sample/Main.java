8eecb5f580242a170dfd8013952434eeac6f0892
William St. Onge <stongew1@mymail.nku.edu>
2020-01-17 20:11:30 -0500

    Init

---
+++ src/sample/Main.java
@@ -0,0 +1,20 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(new GridPane(), 300, 275));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
